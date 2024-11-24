329c157b68ac6b2c20f42ffecc93ae8751222441
ash <ash@schnoor.email>
2024-11-23 21:44:16 -0500

    Add files via upload

---
+++ utils/cachedImages.js
@@ -0,0 +1,31 @@
+import cloudinary from "./cloudinary";
+
+let cachedResults;
+
+export default async function getResults() {
+  if (!cachedResults) {
+    const fetchedResults = await cloudinary.v2.search
+      .expression(`folder:typicalmitul/*`)
+      .sort_by("public_id", "desc")
+      .max_results(400)
+      .execute();
+
+    cachedResults = fetchedResults;
+  }
+
+  return cachedResults;
+}
+
+export async function getMetroResults() {
+  if (!cachedResults) {
+    const fetchedResults = await cloudinary.v2.search
+      .expression(`folder:metro/*`)
+      .sort_by("public_id", "desc")
+      .max_results(400)
+      .execute();
+
+    cachedResults = fetchedResults;
+  }
+
+  return cachedResults;
+}
